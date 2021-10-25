a95022b6b3783ad8a190cc5d658c433c8a743467
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-10-24 19:35:05 -0500

    Implement EchoServer
    This commit implements the EchoServer script.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,33 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket socket = new ServerSocket(portNumber);
+
+            while (true) {
+                Socket client = socket.accept();
+
+                InputStream  clientIn = client.getInputStream();
+                OutputStream clientOut = client.getOutputStream();
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = clientIn.read(buffer)) != -1) {
+                    clientOut.write(buffer, 0 , bytesRead);
+                    clientOut.flush();
+                }
+
+                client.close();
+
+            }
+        } catch(IOException ioe) {
+            System.out.println("We caugt an unexpected exception");
+        }
+    }
 }
