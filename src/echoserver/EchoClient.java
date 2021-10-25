6b1294dfb585c3f35faa7a1e1638bc12cf1c2eb3
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-10-24 19:27:32 -0500

    Add comments to EchoClient
    This commit adds comments to EchoClient for clarity.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -7,7 +7,9 @@ public class EchoClient {
 
     public static void main(String[] args) {
 	    String server;
-
+		
+	    // If no argument provided, make server local host
+	    // Else set server to inital argument
 	    if(args.length == 0) {
 		    server = "127.0.0.1";
 	    }
@@ -16,6 +18,8 @@ public class EchoClient {
 	    }
 
 	    try {
+
+		    // Create socket
 		    Socket socket = new Socket(server, portNumber);
 		    InputStream socketReceive = socket.getInputStream();
 		    OutputStream socketSend = socket.getOutputStream();
@@ -23,21 +27,31 @@ public class EchoClient {
 		    byte[] buffer = new byte[4096];
 		    int bytesRead;
 
+		    // Read data in from user input
 		    while ((bytesRead = System.in.read(buffer)) != -1) {
+
+			    // Send data to server 
 			    socketSend.write(buffer, 0, bytesRead);
 			    socketSend.flush();
 
+			    // Receive data from server and write it out
 			    bytesRead = socketReceive.read(buffer);
 			    System.out.write(buffer, 0, bytesRead);
 			    System.out.flush();
 		    }
 
+
+		    // Tell server we won't be sending anymore output
 		    socket.shutdownOutput();
 
+
+		   // Read whatever data the server has left
 		    while ((bytesRead = socketReceive.read(buffer)) != -1 ) {
 			    System.out.write(buffer, 0, bytesRead);
 			    System.out.flush();
 		    }
+
+		    // Close socket connection
 		    socket.close();
 	    } catch(Exception e) {
 		    System.out.println(e);
